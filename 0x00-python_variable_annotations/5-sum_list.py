56e1d9dd692b6d4b591db02460e04d66fee482d1
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-08-02 19:10:10 +0300

    test

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Sum a list of floats
+"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return sum of list of floats
+    """
+    return sum(input_list)
