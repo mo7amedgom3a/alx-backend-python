ead2fd1713a82604a4bfdabaaf48ae7b0300399b
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-08-06 06:00:12 +0300

    test

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """Asynchronous coroutine that takes in an integer argument"""
+    delays = [wait_random(max_delay) for i in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
