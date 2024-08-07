c157477e96cc27e0a69a64c44b6d74ccf2fd16ba
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-08-07 04:29:10 +0300

    test

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """0. Async Generator"""
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine that yields a random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
