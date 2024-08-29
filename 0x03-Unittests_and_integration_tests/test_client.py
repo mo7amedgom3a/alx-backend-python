3ffae1436683ebf378457d0f8e97383adcdcbe7d
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-08-29 13:03:40 +0300

    test

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""A module for testing the client module.
+"""
+import unittest
+from typing import Dict
+from unittest.mock import (
+    MagicMock,
+    Mock,
+    PropertyMock,
+    patch,
+)
+from parameterized import parameterized, parameterized_class
+from requests import HTTPError
+
+from client import (
+    GithubOrgClient
+)
+
+class TestGithubOrgClient(unittest.TestCase):
+    """_summary_
+
+    Args:
+            unittest (_type_): _description_
+    """
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
+    ])
+    @patch(
+        "client.get_json",
+    )
+    def test_org(self, org: str, expected_response: Dict,
+                 mocked_function: MagicMock) -> None:
+        """_summary_
+
+        Args:
+                org (str): _description_
+                expected_response (Dict): _description_
+                mocked_function (MagicMock): _description_
+        """
+        mocked_function.return_value = MagicMock(
+            return_value=expected_response)
+        goclient = GithubOrgClient(org)
+        self.assertEqual(goclient.org(), expected_response)
+        mocked_function.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
+        )
